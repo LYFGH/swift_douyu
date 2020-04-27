9d045be882b9157cb303352cb77bd14f1b67809c
李亚飞 <yafeigm@gmail.com>
2020-04-27 21:19:34 +0800

    初始化设置

---
+++ swift_douyu/swift_douyu/AppDelegate/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  swift_douyu
+//
+//  Created by 李亚飞 on 2020/4/27.
+//  Copyright © 2020 李亚飞. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+
+
+}
+
---
+++ swift_douyu/swift_douyu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_douyu
+//
+//  Created by 李亚飞 on 2020/4/27.
+//  Copyright © 2020 李亚飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
